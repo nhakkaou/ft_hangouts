8850f4b70ef0a9eaeab3c82aca6be00dbb617281
Noreddine Hakkaoui <nhakkaou@e2r5p7.1337.ma>
2022-04-25 16:24:19 +0000

    Update Navigator

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MyNavigator from "./Screens/MyNavigator";
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyNavigator />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ Screens/Componenets/Avatar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+const Avatar = styled.View`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background-color: #f5f5f5;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+const Avatar = () => {
+  return <Avatar />;
+};
+
+export default Avatar;
