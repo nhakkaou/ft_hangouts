9e5dedf541ad13b870407410163f9ed9c87a901f
nhakkaou <noureddine.hak@gmail.com>
2023-02-28 20:51:59 +0000

    switch to native

--- README.md
+++
@@ -1,2 +0,0 @@
-# ft_hangouts
-This project is designed to get you acquainted with the mobile system. You will have to create a contact management application.
--- Screens/Componenets/Style/AddUser.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Select = styled.Text`
-  font-weight: ${(props) => (props.enabled ? "bold" : 400)};
-  color: ${(props) => (props.color ? props.color : "#000")};
-  text-decoration: ${(props) => (props.enabled ? "underline" : "none")};
-  font-size: ${(props) => (props.enabled ? "15px" : "12px")};
-`;
